ebc6056bc1123ac48f7a5f6c3a7ede7e6066ccf0
st01tkh <st01tkh@gmail.com>
2016-04-06 05:51:53 +0300

    Added the most files

---
+++ files/etc/profile.d/path.sh
@@ -0,0 +1,1 @@
+export PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin
---
+++ manifests/init.pp
@@ -0,0 +1,31 @@
+# == Class: cygwin_common
+#
+# Install CygWin
+#
+# === Parameters
+#
+# === Variables
+#
+# === Examples
+#
+#  class { 'cygwin_common':
+#  }
+#
+# === Authors
+#
+# st01tkh <st01tkh@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2016 st01tkh
+#
+class cygwin_common {
+  if ($operatingsystem == 'windows') {
+    Package { provider => chocolatey, }
+    package { 'cygwin': ensure => installed, } ->
+    package { 'cyg-get': ensure => installed, } ->
+    class {'cygwin_common::path' : }
+  } else {
+      notify {"$operatingsystem is not supported": }
+  }
+}
